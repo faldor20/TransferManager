3f005d947de17d6cdcb83e2dc0e699b0e0cb2e74
eli dowling <eli.jambu@yahoo.com>
2020-06-26 08:34:12 +1000

    converted config format to yaml and made this is of async streams allso an async stream
    i suspectd having a list of streams might have stopped transfers from more than one watchdir starting correctly
    
    
    Former-commit-id: 7ed99ea5329841024a19b86b367cd2e90d60a82f

--- Transfer/Manager.fs
+++ Transfer/Manager.fs
@@ -9,11 +9,13 @@ open FSharp.Json
 open System
 open IOExtensions
 open Data;
+open Legivel.Serialization
 open FSharp.Control
 module Manager =
 
     
     type jsonData = { WatchDirs: WatchDirSimple list }
+    type yamlData = { WatchDirs: WatchDirSimple list }
 
     let mainLoop2 watchDir =
         let tasks = GetNewTransfers2 watchDir 
@@ -30,13 +32,15 @@ module Manager =
         Data.setTransferData { (Data.getTransferData().[id]) with Status=SharedData.TransferStatus.Cancelled; } id
 
     let startUp =
-        let json = try File.ReadAllText("./WatchDirs.json")
-                   with 
-                       |IOException-> printfn "Could not find WatchDirs.json, that file must exist"
-                                      "Failed To open 'WatchDirs.json' file must exist for program to run "
+        let configText = try File.ReadAllText("./WatchDirs.yaml")
+                         with 
+                            |IOException-> printfn "ERROR Could not find WatchDirs.yaml, that file must exist"
+                                           "Failed To open 'WatchDirs.json' file must exist for program to run "
         let watchDirsUnfiltered = 
-            try Json.deserialize<jsonData> json
-            with |JsonDeserializationError -> failwith "Json file malformed, there is an error somewhere"
+            match (Deserialize<yamlData> configText).[0] with
+               |Success data -> printfn "Deserilaization Warnings: %A" data.Warn
+                                data.Data
+               |Error error ->failwith <|sprintf "Config file (%s) malformed, there is an error at %s becasue: %A" configText error.StopLocation.AsString error.Error
         // Here we check if the directry exists by getting dir and file info about the source and dest and
         //filtering by whether it triggers an exception or not
         
@@ -81,47 +85,46 @@ module Manager =
          // The key to this code is having an async seqence for each watchdir.
          // the asyncsequence triggers its map and iter functions for each new item as it is found
          //this means it kind of acts like an event trigger.
-        async{
-            watchDirsData|>List.iter(fun watchDir->printfn "Watching: %A" watchDir )
-                       
-            let schedules = mainLoop2  watchDirsData
+        
+        watchDirsData|>List.iter(fun watchDir->printfn "Watching: %A" watchDir )
+                   
+        let schedules = mainLoop2  watchDirsData
 
-            
-            
-            let completeTransferTasks = schedules|> List.map (fun directoryGroup ->
-                        //This schedules the tasks it needs to be paralell because all transfers should be scheduled immidiatley
-                        //as soon as the file is detected
-                        let scheduledTasks=directoryGroup|>AsyncSeq.mapAsyncParallel(fun scheduleTask->scheduleTask)
-                        //This iterates though the transfer tasks. It is "iterAsync" and not parallel
-                        //becuase we want the transfers to be started one after another
-                        scheduledTasks|> AsyncSeq.iterAsync (fun task ->
-                            async{
-                                let! transResult, id,ct = task
-                                let source = Data.data.[id].Source
-                                match transResult with 
-                                    |TransferResult.Success-> sucessfullCompleteAction id source
-                                    |TransferResult.Cancelled-> CancelledCompleteAction id source
-                                    |TransferResult.Failed-> FailedCompleteAction id source
-                               
-                                let rec del path iterCount= async{
-                                    if iterCount>10 
-                                    then 
-                                        printfn"Error: Could not delete file at after trying for a minute : %s " path
-                                        return ()
-                                    else
-                                        try 
-                                            File.Delete(path) 
-                                        with 
-                                            |_-> do! Async.Sleep(1000)
-                                                 printfn "Error Couldn't delete file, probably in use somehow"
-                                                 do! del path (iterCount+1)
-                                    }
-                                do! del source 0
-                                
+        
+        
+        let completeTransferTasks = asyncSeq{ 
+            for directoryGroup in schedules do
+                //This schedules the tasks it needs to be paralell because all transfers should be scheduled immidiatley
+                //as soon as the file is detected
+                let scheduledTasks=directoryGroup|>AsyncSeq.mapAsyncParallel(fun scheduleTask->scheduleTask)
+                //This iterates though the transfer tasks. It is "iterAsync" and not parallel
+                //becuase we want the transfers to be started one after another
+                yield scheduledTasks|> AsyncSeq.iterAsync (fun task ->
+                     async{
+                        let! transResult, id,ct = task
+                        let source = Data.data.[id].Source
+                        match transResult with 
+                            |TransferResult.Success-> sucessfullCompleteAction id source
+                            |TransferResult.Cancelled-> CancelledCompleteAction id source
+                            |TransferResult.Failed-> FailedCompleteAction id source
+                       
+                        let rec del path iterCount= async{
+                            if iterCount>10 
+                            then 
+                                printfn"Error: Could not delete file at after trying for a minute : %s " path
+                                return ()
+                            else
+                                try 
+                                    File.Delete(path) 
+                                with 
+                                    |_-> do! Async.Sleep(1000)
+                                         printfn "Error Couldn't delete file, probably in use somehow"
+                                         do! del path (iterCount+1)
                             }
-                        ))
-            completeTransferTasks|>List.iter (fun task ->task|>Async.Start)
-                    
-                    //printfn " current state=%A" Data.data
-        }
+                        do! del source 0
+                        
+                    }
+                )}
+        completeTransferTasks|>AsyncSeq.iterAsyncParallel(id)
+    
         
