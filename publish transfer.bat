dc2e738ec1e4570fe3adf5a642545a1fabdf2156
eli dowling <eli.jambu@yahoo.com>
2020-07-03 10:56:55 +1000

    improved te publishing scripts to put the output in the publish dir
    Former-commit-id: 75daa934e6f8d7f50e1d62875630db3500154b05

--- publish transfer.bat
+++ publish transfer.bat
@@ -1,2 +1,3 @@
 cd "./Transfer/"
 dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true --no-self-contained
+robocopy "./bin/Release/netcoreapp3.1/win-x64/publish" "../Publish/TransferManager/" /s 
