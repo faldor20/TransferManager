27b57a7be80066750ff847e60fdb22ea6b6064a3
eli dowling <eli.jambu@yahoo.com>
2020-09-02 10:35:41 +1000

    added system for checking if files on an ftp server are still growing.
    we use the modified date to find that out
    
    
    Former-commit-id: 7d3f748ecdb471fb1214e36356667c6aff0ab046

--- Transfer/TransferClient/Scheduler.fs
+++ Transfer/TransferClient/Scheduler.fs
@@ -7,38 +7,76 @@ open TransferClient.DataBase.TokenDatabase
 open TransferClient.IO.Types
 open SharedFs.SharedTypes
 open DataBase.Types
-open Logary
 open TransferClient.IO
+open FluentFTP
 module Scheduler =
+    type Availability=
+    |Available=0
+    |Deleted=1
+    |Cancelled=2
     //This will return once the file is not beig acessed by other programs.
     //it returns false if the file is discovered to be deleted before that point.
-    let isAvailable (source:string) =
+    let isAvailable (source:string) (ct:CancellationToken) =
         async {
             let fileName= Path.GetFileName (string source)
             let mutable currentFile = new FileInfo(source)
-            let mutable unavailable = true
-            let mutable fileExists=false
-            while unavailable do
+            let mutable loop = true
+            let mutable availability=Availability.Available
+            while loop do
+                if ct.IsCancellationRequested then
+                    availability<- Availability.Cancelled
+                    loop<- false
                 try
                     using (currentFile.Open(FileMode.Open, FileAccess.Read, FileShare.None)) (fun stream ->
                         stream.Close())
-                    fileExists<-true
-                    unavailable<-false
+                    availability<-Availability.Available
+                    loop<-false
                 with 
                     | :? FileNotFoundException | :? DirectoryNotFoundException  ->
                         printfn "%s deleted while waiting to be available" fileName
-                        fileExists<-false
-                        unavailable<-false
+                        availability<-Availability.Deleted
+                        loop<-false
                     | :? IOException ->
+                        
                         do! Async.Sleep(1000)
+
                     | ex  ->
                         printfn "file failed with %A" ex.Message
-                        unavailable<- false
-                        fileExists<-false
-            return fileExists
+                        loop<- false
+                        availability<-Availability.Deleted
+            return availability
         }
-        //i think this has some kind of overflow still
-    let isAvailable2 source=
+    let isAvailableFTP (ct:CancellationToken) (client:FtpClient)(source:string)  =
+            async {
+                
+                let mutable loop = true
+                let mutable availability=Availability.Available
+                let name=Path.GetFileName(source)
+                let directory=Path.GetDirectoryName(source)
+                let mutable modifiedDate=client.GetModifiedTime (source)
+                while loop do
+                    do! Async.Sleep 1000
+                    if ct.IsCancellationRequested then
+                        availability<- Availability.Cancelled
+                        loop<- false
+                    else
+                        try 
+                            let! newModDate = Async.AwaitTask<|client.GetModifiedTimeAsync(source,token= ct)
+                            if modifiedDate=  newModDate then
+                                availability<-Availability.Available
+                                loop<-false
+                             else
+                                modifiedDate<-newModDate
+                                
+                        with|ex->
+                            Logging.errorf "{Scheduler} Error thrown during availability check %A" ex
+                            loop<-false
+                            availability<-Availability.Deleted
+
+                return availability
+            }
+        //i think this has some kind of overflow 
+    let isAvailable2 source (ct:CancellationToken)=
         async{
             let rec loop (currentFile:FileInfo)  = 
                 async {
@@ -46,15 +84,17 @@ module Scheduler =
                     try
                         using (currentFile.Open(FileMode.Open, FileAccess.Read, FileShare.None)) (fun stream ->
                             stream.Close())
-                        return true
+                        return Availability.Available
                     with 
                         | :? FileNotFoundException->
-                             return false
+                             return Availability.Deleted
                         | :? IOException ->
-                            do! Async.Sleep(1000)
-                            return! loop(currentFile)
+                            if ct.IsCancellationRequested then
+                                return Availability.Cancelled
+                            else
+                                return! loop(currentFile)
                         |_->failwith "something went very wrong while waiting for a file to become available"
-                            return false
+                            return Availability.Deleted
                 }
             return! loop(FileInfo(source))
         }
@@ -101,19 +141,26 @@ module Scheduler =
             //Doesn't work on ftp files
             let fileAvailable=
                 match moveData.SourceFTPData with
-                    |Some _-> true
-                    |None-> Async.RunSynchronously( isAvailable file.Path)
+                    |Some ftpData-> 
+                        let client=IO.FTPMove.ftpClient ftpData
+                        client.Connect()
+                        Async.RunSynchronously<| (file.Path|>isAvailableFTP  ct.Token client)
+                    |None-> Async.RunSynchronously( isAvailable file.Path ct.Token)
         
             let transDataAccess= TransDataAcessFuncs dbAccess groupName index
 
-            if fileAvailable then
+            if fileAvailable= Availability.Available then
                 Logging.infof "{Available} file at: %s is available" logFilePath 
 
                 dbAccess.Set groupName index (getFileData file (dbAccess.Get groupName index) )  
                 
                 return Mover.MoveFile file.Path moveData transDataAccess transcode  ct
-            else
-                Logging.warnf "{Deleted} While waiting to be available Transfer file at: %s" logFilePath 
+            else if fileAvailable =Availability.Deleted then
+                Logging.warnf "{Scheduler} File Deleted  while waiting to be available Transfer file at: %s" logFilePath 
                 dbAccess.Set groupName index {dbAccess.Get groupName index with Status=TransferStatus.Failed} 
                 return async{ return (Types.TransferResult.Failed, transDataAccess,moveData.DirData.DeleteCompleted)}
+            else 
+                Logging.infof "{Scheduler} File cancelled while waiting to be available Transfer file at: %s" logFilePath 
+                dbAccess.Set groupName index {dbAccess.Get groupName index with Status=TransferStatus.Cancelled} 
+                return async{ return (Types.TransferResult.Cancelled, transDataAccess,moveData.DirData.DeleteCompleted)}
         }
