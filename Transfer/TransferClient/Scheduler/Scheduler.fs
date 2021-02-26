04eb5f29a53ca23b5c07a6e94b4e5c6b87264924
eli dowling <eli.jambu@yahoo.com>
2021-02-26 12:01:53 +1000

    Moved some of scheduler code to a  seperate fie
    Former-commit-id: 3a266ed663a680505ca565b97974e8134a407eab

--- Transfer/TransferClient/Scheduler/Scheduler.fs
+++ Transfer/TransferClient/Scheduler/Scheduler.fs
@@ -10,100 +10,8 @@ open TransferClient.IO
 open FluentFTP
 open TransferClient.JobManager
 open JobManager.Main
+open Scheduler.AvailabilityChecker
 module Scheduler =
-    type Availability=
-    |Available=0
-    |Deleted=1
-    |Cancelled=2
-    //This will return once the file is not beig acessed by other programs.
-    //it returns false if the file is discovered to be deleted before that point.
-    let isAvailable (source:string) (ct:CancellationToken) =
-        async {
-            let fileName= Path.GetFileName (string source)
-            let mutable currentFile = new FileInfo(source)
-            let mutable loop = true
-            let mutable availability=Availability.Available
-            while loop do
-                if ct.IsCancellationRequested then
-                    availability<- Availability.Cancelled
-                    loop<- false
-                try
-                    using (currentFile.Open(FileMode.Open, FileAccess.Read, FileShare.None)) (fun stream ->
-                        stream.Close())
-                    availability<-Availability.Available
-                    loop<-false
-                with 
-                    | :? FileNotFoundException | :? DirectoryNotFoundException  ->
-                        printfn "%s deleted while waiting to be available" fileName
-                        availability<-Availability.Deleted
-                        loop<-false
-                    | :? IOException ->
-                        
-                        do! Async.Sleep(1000)
-
-                    | ex  ->
-                        printfn "file failed with %A" ex.Message
-                        loop<- false
-                        availability<-Availability.Deleted
-            return availability
-        }
-    let isAvailableFTP (ct:CancellationToken) (client:FtpClient)(source:string)  =
-            async {
-                
-                let mutable loop = true
-                let mutable availability=Availability.Available
-                let name=Path.GetFileName(source)
-                let directory=Path.GetDirectoryName(source)
-                let mutable modifiedDate=client.GetModifiedTime (source)
-                while loop do
-                    do! Async.Sleep 1000
-                    if ct.IsCancellationRequested then
-                        availability<- Availability.Cancelled
-                        loop<- false
-                    else
-                        try 
-                            if not (client.FileExists(source)) then failwith "" 
-                            let! newModDate = Async.AwaitTask<|client.GetModifiedTimeAsync(source,token= ct)
-                            if modifiedDate=  newModDate then
-                                try
-                                    let read=client.OpenRead(source)
-                                    availability<-Availability.Available
-                                    loop<-false
-                                    read.Close()
-                                with|_->()
-                             else
-                                modifiedDate<-newModDate
-                                
-                        with|ex->
-                            Logging.errorf "{Scheduler} Error thrown during availability check %A" ex
-                            loop<-false
-                            availability<-Availability.Deleted
-
-                return availability
-            }
-        //i think this has some kind of overflow 
-    let isAvailable2 source (ct:CancellationToken)=
-        async{
-            let rec loop (currentFile:FileInfo)  = 
-                async {
-                    do! Async.Sleep(1000)
-                    try
-                        using (currentFile.Open(FileMode.Open, FileAccess.Read, FileShare.None)) (fun stream ->
-                            stream.Close())
-                        return Availability.Available
-                    with 
-                        | :? FileNotFoundException->
-                             return Availability.Deleted
-                        | :? IOException ->
-                            if ct.IsCancellationRequested then
-                                return Availability.Cancelled
-                            else
-                                return! loop(currentFile)
-                        |_->failwith "something went very wrong while waiting for a file to become available"
-                            return Availability.Deleted
-                }
-            return! loop(FileInfo(source))
-        }
     let getFileData (file:FoundFile) currentTransferData=
         let fileSize=
             match file.FTPFileInfo with 
@@ -113,9 +21,8 @@ module Scheduler =
                 (new FileInfo(file.Path)).Length
         let fileSizeMB=(float(fileSize/int64 1000))/1000.0
 
-        {currentTransferData with
-                FileSize=fileSizeMB
-        }
+        {currentTransferData with FileSize=fileSizeMB}
+
     let transferType moveData=
                 let{SourceFTPData= source; DestFTPData=dest;}=moveData
                 match (source,dest) with
@@ -123,6 +30,7 @@ module Scheduler =
                 |None,Some _->TransferTypes.LocaltoFTP
                 |Some _,None->TransferTypes.FTPtoLocal
                 |None,None->TransferTypes.LocaltoLocal
+
     let makeTransData moveData sourceFilePath file id:TransferData=
                 getFileData 
                     file
@@ -139,6 +47,7 @@ module Scheduler =
                       Status = TransferStatus.Unavailable 
                       ScheduledTime=DateTime.Now
                       EndTime=new DateTime()}
+                      
     let scheduleTransfer (file:FoundFile) moveData (dbAccess:Access.DBAccess) transcode =
         async {
             let sourceID=(List.last(moveData.GroupList))
@@ -154,13 +63,8 @@ module Scheduler =
             
             let makeJob id={Job=Mover.MoveFile file.Path moveData dbAccess id transcode ct; SourceID=sourceID; ID=id; Available=false; TakenTokens=List.Empty ;CancelToken=ct}
 
-            let jobID= 
-                dbAccess.AddJob
-                    (sourceID)
-                    makeJob
-                    makeTrans
+            let jobID= dbAccess.AddJob (sourceID) makeJob makeTrans
             
-
             let setStatus status data =
                    {data with Status=status} 
             let updateTransData data =
