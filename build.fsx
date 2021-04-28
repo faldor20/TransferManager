3c1cbf9e5c07544e5ad9b83426bb6e77fffe7b51
Eli Dowling <eli.jambu@yahoo.com>
2021-04-28 12:53:20 +1000

    Improved build code. Added ability to publish individual components.

--- build.fsx
+++ build.fsx
@@ -30,26 +30,34 @@ let buildProject target output selfContained projectFile  =
                       Runtime = Some target })
     with ex -> printfn "failed to build %s: reason %A" projectFile ex
 
+
+let buildProject2 pathext source target outputPath selfContained   =
+    let output= outputPath+pathext
+    [output]|> Shell.cleanDirs
+    source|>buildProject target output selfContained
+
+let managerProj="src/Transfer/ClientManager/ClientManager.fsproj"
+let clientProj="src/Transfer/ClientManager/ClientManager.fsproj"
+let webUIProj="src/WebUI/Server/WebUI.Server.csproj"
+
+let buildManager =
+     buildProject2 "/Transfer/ClientManager" managerProj
+
+let buildClient =
+     buildProject2 "/Transfer/TransferClient" clientProj
+
+let buildWebUI =
+     buildProject2 "/WebServer" webUIProj
+
 let buildProjects target outputPath selfContained =
-    let managerOutput = outputPath + "Transfer/Clientmanager"
-    let clientOutput = outputPath + "/Transfer/TransferClient"
-    let webOutput = outputPath + "/WebServer"
-
-    [ managerOutput
-      clientOutput
-      webOutput ]
-    |> Shell.cleanDirs
-    
-    "src/Transfer/ClientManager/ClientManager.fsproj"
-    |> buildProject target managerOutput selfContained
-
-    "src/Transfer/TransferClient/TransferClient.fsproj"
-    |> buildProject target clientOutput selfContained
-
-    "src/WebUI/Server/WebUI.Server.csproj"
-    |> buildProject target webOutput true
+    buildManager target outputPath selfContained
+    buildClient target outputPath selfContained
+    buildWebUI target outputPath true
 
 Target.create "PubLinux" (fun _ -> buildProjects "linux-x64" "./Publish-linux/" true)
+Target.create "PubMan-Lin" (fun _ -> buildManager "linux-x64" "./Publish-linux/" true )
+Target.create "PubClient-Lin" (fun _ -> buildClient "linux-x64" "./Publish-linux/" true )
+Target.create "PubWeb-Lin" (fun _ -> buildWebUI "linux-x64" "./Publish-linux/" true )
 
 Target.create "PubWin" (fun _ -> buildProjects "win-x64" "./Publish-Win/" false)
 
