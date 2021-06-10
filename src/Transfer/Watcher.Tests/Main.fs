dcc2e77c59b8e6738451f0e774ec3c60b3d3d3b4
Eli Dowling <eli.jambu@yahoo.com>
2021-06-10 10:32:00 +1000

    Added Watcher.Tests project for testing the File Watcher and availability checker

---
+++ src/Transfer/Watcher.Tests/Main.fs
@@ -0,0 +1,6 @@
+﻿module Watcher.Tests
+open Expecto
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
--- src/Transfer/Watcher.Tests/Sample.fs
+++ src/Transfer/Watcher.Tests/Sample.fs
@@ -1,12 +1,12 @@
-﻿module Availability
-open TransferClient.Scheduling.AvailabilityChecker
+module Tests
+
+open Expecto
 
 open System
 open System.Runtime
 open System.IO
-open Expecto
 open System.Threading
-open FSharp.Control.Tasks.V2
+open FileWatcher.AvailabilityChecker
 
 let runintestEnv fn=
     let a=Directory.CreateDirectory ("./testDir")
@@ -19,53 +19,52 @@ let runintestEnv fn=
         Directory.Delete( "./testDir",true)
 ///Writes to the stream for an amount of tie and then disposes it
 let writeIntermittently (file:string) (interval:int) numtimes=
-    task{
+    async{
         use a = File.AppendText(file)
         printfn "about to start writing"
         for i in 0..numtimes do
             a.WriteLine "heyy"
+            a.Flush()
             do! Async.Sleep(interval)
+        return System.Diagnostics.Stopwatch.GetTimestamp()
     }
     
-let simplecheck name (time:int)=
+(* let simplecheck name (time:int)=
     let mutable cont=true
     let mutable lastInfo=new FileInfo(name)
     while cont do
+       printfn "n old = %i " lastInfo.Length
        Async.RunSynchronously <|Async.Sleep(time)
        let newInfo= new FileInfo(name)
        printfn "new=%i old = %i "newInfo.Length lastInfo.Length
        if newInfo.Length= lastInfo.Length then
-            cont<-false
+            cont<-false *)
     
 let testInterval checker writeInterval writes (checkInterval:int)  =
-    let task=
-        task{
-            let name= "./testfile.test"
-            let a=File.WriteAllText(name,"sart")
-            let source= CancellationTokenSource()
-            let ct= source.Token
-            source.CancelAfter(writeInterval*writes+(checkInterval*3))
-            let writer= (writeIntermittently name writeInterval writes)
-            //let! checker= Async.StartAsTask <| checkAvailability checker name ct checkInterval
-            simplecheck name checkInterval
-            if ct.IsCancellationRequested then 
-                failwith "Took to long and had to call cancelation on checking. this means the check never sees it is avaialable"
-            if writer.IsCompleted then 
-                writer.Dispose()
-                return true
-            else 
-                writer.Wait()
-                writer.Dispose()
-                return false
-        }
-    task.Wait()
-    task.Result
+    async{
+        let name= "./testfile.test"
+        let a=File.WriteAllText(name,"sart")
+        let source= new CancellationTokenSource()
+        let ct= source.Token
+        source.CancelAfter(writeInterval*(writes+1)+(checkInterval*3))
+        let! writer=Async.StartChild (writeIntermittently name writeInterval writes)
+        let! checker= checkAvailability checker name ct checkInterval
+        let availableTime=Diagnostics.Stopwatch.GetTimestamp()
+        if ct.IsCancellationRequested then 
+            failwith "Took to long and had to call cancelation on checking. this means the check never sees it is avaialable"
+        let! writeFinished= writer
+        if availableTime>writeFinished then 
+            return true
+        else 
+            return false
+    }|>Async.RunSynchronously
+    
 ///Test the availability checker with an interval that is to short and will result in the checker thinking he write is complete 
 let testIncorrectInterval checker  =
-        testInterval checker 500 20 300
+        testInterval checker 150 10 90
 ///Test the availability checker with the apropriate checking interval
 let testCorrectInterval checker  =
-        testInterval checker 500 20 2100
+        testInterval checker 100 10 150
 
 [<Tests>]
 let test=
@@ -104,22 +103,26 @@ let test=
         test "availability file size check correct interval"{
             let res=  (fun ()-> testCorrectInterval checkFileSize)
                 
-            Expect.isTrue(res()) "File was considered available after writer had finished writing"
+            Expect.isTrue(runintestEnv res) "File was considered available after writer had finished writing"
         };
         test "availability file size check incorrect interval"{
             let res=  (fun ()-> testIncorrectInterval checkFileSize)
                 
-            Expect.isFalse(res()) "File was considered available during writing, when the checking interval was too small, as expected"
+            Expect.isFalse(runintestEnv res) "File was considered available during writing, when the checking interval was too small, as expected"
         }
         test "availability open Stream check correct interval"{
             let res=  (fun ()-> testCorrectInterval checkFileStream)
         
-            Expect.isTrue(res()) "File was considered available after writer had finished writing"
+            Expect.isTrue(runintestEnv res) "File was considered available after writer had finished writing"
         };
         test "availability open Stream size check incorrect interval"{
             let res=  (fun ()-> testIncorrectInterval checkFileStream)
         
-            Expect.isTrue(res()) "File Stream check not effectedbyt to small interval"
+            Expect.isTrue(runintestEnv res) "File Stream check not effectedbyt to small interval"
+        };
+        test "availability size and write time"{
+            let res=  (fun ()-> testCorrectInterval checkSizeandWriteTime)
+        
+            Expect.isTrue(runintestEnv res) "File Stream check not effectedbyt to small interval"
         }
     ]
-
